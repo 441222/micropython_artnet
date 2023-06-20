77889d3e3e4869fa291eeb528f3508919ddc77f1
441222 <6120nashville@gmail.com>
2023-06-20 16:57:17 +0900

    Update artnet_recv.py

--- artnet_recv.py
+++ artnet_recv.py
@@ -14,16 +14,20 @@ class udprecv():
         self.SrcAddr = (SrcIP, SrcPort)                # アドレスをtupleに格納
         self.BUFSIZE = 1024                            # バッファサイズ指定
         self.udpServSock = socket(AF_INET, SOCK_DGRAM) # ソケット作成
+        self.udpServSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.udpServSock.bind(self.SrcAddr)            # 受信元アドレスでバインド
 
     def recv(self):
         packe,addr=self.udpServSock.recvfrom(self.BUFSIZE)
         data = [int(b) for b in packe]
         #del data[0:18]
-        if data[14] == universe:
-            del data[0:18]
-            print(data)
-
+        try:
+            if data[14] == universe:
+                del data[0:18]
+                print(data)
+        except IndexError:
+            pass
+        
 udp = udprecv()
 while True:
     udp.recv()
