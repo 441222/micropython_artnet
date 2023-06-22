fb833889593a338c036013e99ef288eb417a4ca4
441222 <6120nashville@gmail.com>
2023-06-23 06:57:56 +0900

    fixed

--- artnet2np.py
+++ artnet2np.py
@@ -1,29 +1,26 @@
-import usocket as socket
+from usocket import *
 import machine, neopixel
 import network
 
-SSID = 'SSID'
-PASSWORD = 'パスコード'
 host = '192.168.1.27' # 送信元IPアドレス
 universe = 0 # universe番号
 
-led_length = 240 #ledの数
+total = 240 #ledの数
 led_num = 3 #何こづつ制御するか
 
-np = neopixel.NeoPixel(machine.Pin(10), led_length) #neopixelの設定
-
+np = neopixel.NeoPixel(machine.Pin(10), total) #neopixelの設定
 
 def wifi_startup():
     sta_if = network.WLAN(network.STA_IF)
     if sta_if.active(True):
-        sta_if.connect(SSID, PASSWORD)
+        sta_if.connect('ssid','paass')
         while not sta_if.isconnected():
             pass
         sta_if.ifconfig(('192.168.1.11', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
 
 class udprecv():
     def __init__(self):
-        SrcIP  = gethostbyname(gethostbyname(host))    # 送信元IPアドレス
+        SrcIP  = host    # 送信元IPアドレス
         SrcPort = 6454                                 # 受信元ポート番号
         self.SrcAddr = (SrcIP, SrcPort)                # アドレスをtupleに格納
         self.BUFSIZE = 1024                            # バッファサイズ指定
@@ -32,21 +29,22 @@ class udprecv():
 
     def recv(self):
         packe,addr=self.udpServSock.recvfrom(self.BUFSIZE) #受信
-
         data = [int(b) for b in packe] #byte型をint型に変換
 
         try:
             if data[14] == universe:       #universe番号が一致したらneopixelに送信
                 del data[0:18]
                 print(data)
-            for i in range (led_length//led_num):
-                for j in range(led_num):
-                    np[i+j] = (data[i*3], data[i*3+1], data[i*3+2])
+                for i in range(total/led_num): 
+                    for j in range(led_num):
+                        np[i*led_num+j] = (data[i*led_num+j*3], data[i*led_num+j*3+1], data[i*led_num+j*3+2]) 
                 np.write()
         except IndexError:
             pass
+
 
 wifi_startup()
 udp = udprecv()
 while True:
     udp.recv()
+
