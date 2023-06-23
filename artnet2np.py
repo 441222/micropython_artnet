01c62a96237f8337bcedf71ced2c29f24b71c3d4
ootani yoshiyuki <ootanl@ootaninoMacBook-Pro.local>
2023-06-23 14:06:01 +0900

    fixed

--- artnet2np.py
+++ artnet2np.py
@@ -2,18 +2,21 @@ from usocket import *
 import machine, neopixel
 import network
 
+SSID = 'ssid' # 接続先SSID
+PASS = 'pass' # 接続先パスワード
+
 host = '192.168.1.27' # 送信元IPアドレス
 universe = 0 # universe番号
 
-total = 240 #ledの数
+led_length = 240 #ledの数
 led_num = 3 #何こづつ制御するか
 
-np = neopixel.NeoPixel(machine.Pin(10), total) #neopixelの設定
+np = neopixel.NeoPixel(machine.Pin(10), led_length) #neopixelの設定
 
 def wifi_startup():
     sta_if = network.WLAN(network.STA_IF)
     if sta_if.active(True):
-        sta_if.connect('ssid','paass')
+        sta_if.connect(SSID, PASS)
         while not sta_if.isconnected():
             pass
         sta_if.ifconfig(('192.168.1.11', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
@@ -35,7 +38,7 @@ class udprecv():
             if data[14] == universe:       #universe番号が一致したらneopixelに送信
                 del data[0:18]
                 print(data)
-                for i in range(total/led_num): 
+                for i in range(led_length/led_num): 
                     for j in range(led_num):
                         np[i*led_num+j] = (data[i*led_num+j*3], data[i*led_num+j*3+1], data[i*led_num+j*3+2]) 
                 np.write()
